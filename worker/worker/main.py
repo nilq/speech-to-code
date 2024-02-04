778b24dbd3d70ee1ea7e6924f5aec25471aa899f
Niels Horn <slein.nroh@gmail.com>
2024-02-04 19:28:06 +0100

    Fix worker

--- worker/worker/main.py
+++ worker/worker/main.py
@@ -4,41 +4,24 @@ from azure.storage.blob.aio import BlobServiceClient
 from azure.identity import DefaultAzureCredential
 from azure.storage.queue import QueueServiceClient
 
-import asyncio
 
 credential = DefaultAzureCredential(managed_identity_client_id="f1fa9ae3-9815-465f-8a41-26a731203e31")
-storage_account_url = "https://speech46c96a79acf72d79.blob.core.windows.net"
-blob_service_client = BlobServiceClient(account_url=storage_account_url, credential=credential)
-queue_service_client = QueueServiceClient(account_url=storage_account_url, credential=credential)
 
+storage_blob_account_url = "https://speech46c96a79acf72d79.blob.core.windows.net"
+storage_queue_account_url = "https://speech46c96a79acf72d79.queue.core.windows.net"
 
-try:
-    blob_client = blob_service_client.get_blob_client(container="content", blob="testfromworker")
-    with open(__file__, "rb") as data:
-        asyncio.run(blob_client.upload_blob(data))
-except:
-    print("Continuing tests.")
+blob_service_client = BlobServiceClient(account_url=storage_blob_account_url, credential=credential)
+queue_service_client = QueueServiceClient(account_url=storage_queue_account_url, credential=credential)
 
-
-async def process_queue():
-    queue_name = "speechprocessing"
+def process_queue(queue_name: str = "speechprocessing"):
     queue_client = queue_service_client.get_queue_client(queue_name)
 
-    print("QUEUE CLIENT")
-    print(queue_client)
-
-    print("CREDENTIAL")
-    print(credential)
-
-    async with queue_client:
-        while True:
-            messages = await queue_client.receive_messages()
-            for message in messages:
-                file_url = message.content
-                print(f"Received message: {file_url!r}")
-                await queue_client.delete_message(message)
-            await asyncio.sleep(1)
-
+    while True:
+        messages = queue_client.receive_messages(max_messages=5)
+        for message in messages.by_page():
+            file_url = message.content
+            print(f"Received message: {file_url!r}")
+            queue_client.delete_message(message)
 
 def start() -> None:
-    asyncio.run(process_queue())
+    process_queue()
